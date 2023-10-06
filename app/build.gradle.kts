cd0402a751e01edf1f8ddfebd2b0e56de1745abf
ShaikAbdulRazzaaq <shaik.11715485@lpu.in>
2023-10-07 02:05:35 +0530

    Added Motion Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
     implementation("com.google.dagger:hilt-android:2.48.1")
-    implementation("com.airbnb.android:lottie:6.1.0")
     ksp("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("com.airbnb.android:lottie:6.1.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
